dc5dbd2c0c79502b9a68b8796f120b69e5152ea7
Skeeley <stephenlkeeley@gmail.com>
2020-03-20 18:32:16 -0400

    computed LL for CA emissions object

--- CA_Emissions.py
+++ CA_Emissions.py
@@ -23,7 +23,7 @@ class CA_Emissions():
 
     def __init__(
         self,
-        dtStim: float = 0.03333333,
+        dt: float = 0.03333333,
         tau: int = 1,
         alpha: int = 1,
         max_spk: int = 10,
@@ -36,8 +36,8 @@ class CA_Emissions():
 
 
 
-        if dtStim <= 0:
-            raise ValueError("dtStim dim must be positive, got {0}".format(dtStim))
+        if dt <= 0:
+            raise ValueError("dt must be positive, got {0}".format(dt))
         if tau <= 0:
             raise ValueError("Calcium trace timescale must be positive, got {0}".format(tau))
         if alpha <= 0.0:
@@ -46,7 +46,7 @@ class CA_Emissions():
             raise ValueError("max_spk must be positive integer, got {0}".format(max_spk))
 
 
-        self.dtStim = dtStim
+        self.dt = dt
         self.tau = tau
         self.alpha = alpha
         self.max_spk = max_spk
@@ -65,7 +65,7 @@ class CA_Emissions():
 
     @property
     def params(self):
-        return self.dtStim, self.alpha, self.Gauss_sigma, self.T, self.AR1, self.AR2  
+        return self.dt, self.alpha, self.Gauss_sigma, self.T, self.AR1, self.AR2  
 
     def set_data(self, data):
         if np.shape(data)[-1] != self.T:
@@ -74,7 +74,7 @@ class CA_Emissions():
 
 
 
-    def log_likelihood(self,rate, S = 10):
+    def log_likelihood(self,rate_param, S = 10):
         try:
             self.data()
         except AttributeError:
@@ -87,12 +87,42 @@ class CA_Emissions():
             f1 =  b + self.data[:-1,None]
             f0 =  b  + self.data[1:,None]
 
+
+            spk_vec = np.arange(S+1) 
+            spk_mat =spk_vec*np.ones(np.shape(f1))
+            mu = f1*np.exp(-self.dt/self.tau)+self.alpha*spk_mat
+            self.Gauss_ll = log_gaussian_1D(f0, mu, self.Gauss_sigma) #set Gaussian part
+
+
+            ##### do Poiss part 
+            rate = self.mean(rate_param) #convert to rate given dt
+
+            #  Compute Poisson log-probability for each rate, for each spike count in spkvect
+            logpoisspdf_mat = log_poisson_1D(spk_vec, rate)
+
+
+
+            #  Compute joint log-probability of spike counts and Gaussian noise
+            logpjoint = logpoisspdf_mat + self.Gauss_ll
+
+
+
+            loglivec = np.logsumexp(logpjoint, axis = 1)
+
+            #  sum up over time bins
+            neglogli = -np.sum(loglivec) # sum up and take negative 
+
+            return(neglogli)
+
         elif self.AR2:
 
             b = np.zeros(self.T-2,1+S);
             f2 =  b + self.data[:-2]
             f1 =  b + self.data[2:-1]
             f0 =  b + self.data[3:]
+
+
+            ##### DO AR2 PROCESS HERE!
 
 
 
